e80c56f003a2593475aafedd6a19392af28e6813
Brian Kimble <brian@stickbuilt.us>
2025-10-18 03:06:21 +0000

    make this input just a little bit taller (_for element <Input>_)

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -292,7 +292,7 @@ const SidebarInput = React.forwardRef<
       ref={ref}
       data-sidebar="input"
       className={cn(
-        "h-8 w-full bg-background shadow-none focus-visible:ring-2 focus-visible:ring-sidebar-ring",
+        "h-9 w-full bg-background shadow-none focus-visible:ring-2 focus-visible:ring-sidebar-ring",
         className
       )}
       {...props}
