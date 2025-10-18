31599b0c1063b025e0803c6ec56657bec2b02abb
Brian Kimble <brian@stickbuilt.us>
2025-10-18 03:59:34 +0000

    make the background for this button red and icon white as base and then

--- src/components/team-manager.tsx
+++ src/components/team-manager.tsx
@@ -117,8 +117,8 @@ export default function TeamManager() {
                                 </Button>
 
                            )}
-                            <Button variant="ghost" size="icon" className="h-8 w-8 hover:bg-destructive/20" onClick={() => handleDeleteMember(member.id)}>
-                                <Trash2 className="h-4 w-4 text-destructive" />
+                            <Button variant="destructive" size="icon" className="h-8 w-8" onClick={() => handleDeleteMember(member.id)}>
+                                <Trash2 className="h-4 w-4" />
                             </Button>
                         </div>
                     </div>
